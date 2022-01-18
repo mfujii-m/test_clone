bcfd1de6ce45f6cb9c1f5c0dfeb24fb8e440cc45
Masanori Fujii <97643864+mfujii-m@users.noreply.github.com>
2022-01-19 05:40:27 +0900

    create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def func(a, b):
+  return a + b
+
+
+if __name__ == '__main__':
+  args = sys.argv
+  if len(args) >= 2:
+    print(f"a={argv[1]}")
+    print(f"b={argv[2]}")
+    c = func(x,b)
+    print(f"add(a,b)={c}"
+  else:
+    print("usage: cmd a b")
