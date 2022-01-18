51f55ce8af5054fee92b89feecdff4210d1ad0ab
Masanori Fujii <97643864+mfujii-m@users.noreply.github.com>
2022-01-19 05:46:00 +0900

    chg argv to args

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def func(a, b):
 if __name__ == '__main__':
     args = sys.argv
     if len(args) >= 2:
-        print(f"a={argv[1]}")
-        print(f"b={argv[2]}")
+        print(f"a={args[1]}")
+        print(f"b={args[2]}")
         c = func(x,b)
         print(f"add(a,b)={c}")
     else:
