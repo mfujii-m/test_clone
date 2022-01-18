ec19706d62fe054b6a7fd6c238be73971f1982d4
Masanori Fujii <97643864+mfujii-m@users.noreply.github.com>
2022-01-19 05:47:17 +0900

    fix a,b

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def func(a, b):
 if __name__ == '__main__':
     args = sys.argv
     if len(args) >= 2:
-        print(f"a={args[1]}")
-        print(f"b={args[2]}")
-        c = func(x,b)
+        a = args[1]
+        b = args[2]
+        print(f"a={a}")
+        print(f"b={b}")
+        c = func(a,b)
         print(f"add(a,b)={c}")
     else:
         print("usage: cmd a b")
